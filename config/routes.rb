caf5747c4a7d858755030cfd29e1791e275fbd8f
jerico-digiteer <jerico.digiteer@gmail.com>
2024-10-02 12:05:22 +0800

    added admin landing page and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   root "posts#feed"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+      resources :posts
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
